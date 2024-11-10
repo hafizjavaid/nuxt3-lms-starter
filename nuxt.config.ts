fc0c79f1b26f27a87431e28ba8e4842da89f774a
Hafiz Javaid Iqbal <hafizjaidi786@gmail.com>
2024-11-10 14:58:40 +0500

    Nuxt Cloudinary Setup

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,5 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
-  modules: ['@nuxt/icon', '@nuxt/ui']
+  modules: ['@nuxt/icon', '@nuxt/ui', "@nuxtjs/cloudinary"],
 })
